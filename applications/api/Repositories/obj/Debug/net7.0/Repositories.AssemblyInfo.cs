65edf31ea825731c4fa4283eb22e1770d922a217
PlayingWithFire <chandrajanuka@gmail.com>
2025-09-19 14:56:37 +0700

    git init commit

--- applications/api/Repositories/obj/Debug/net7.0/Repositories.AssemblyInfo.cs
+++ applications/api/Repositories/obj/Debug/net7.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ba6c41be773230d50635f1195f34728832e42f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
